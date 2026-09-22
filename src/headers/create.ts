360cec1b0d0220b84666ca14a52148ae57de0975
Ryan Luong <lumintriet@gmail.com>
2026-09-22 10:06:17 +0700

    feat(headers): add return type for createHeaders function.

--- src/headers/create.ts
+++ src/headers/create.ts
@@ -1,6 +1,8 @@
 import type { HttpHeader } from "@little-nebulae/http";
 
-export function createHeaders(init: { [header in HttpHeader]: string }) {
+export function createHeaders(init: {
+  [header in HttpHeader]: string;
+}): Headers {
   const headers = new Headers(init);
   return headers;
 }
