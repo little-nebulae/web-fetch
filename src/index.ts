32968ee462846c7f0b1fcbf29f4713870907a5fa
Ryan Luong <lumintriet@gmail.com>
2026-09-22 10:05:11 +0700

    feat: define lib entry.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+export * from "@/headers/create";
+
+export * from "@/request/create";
+export * from "@/request/create-json";
+
+export * from "@/response/parse-text-body";
+export * from "@/response/parse-json-body";
+
+export * from "@/fetch/response";
+export * from "@/fetch/json";
