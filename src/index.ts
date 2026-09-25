fb1ed1080e2377a224a02835a0c3b2a6dc992f7a
Ryan Luong <lumintriet@gmail.com>
2026-09-25 08:49:00 +0700

    feat: add 2 function to lib entry.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * from "@/headers/create";
 
 export * from "@/request/create";
 export * from "@/request/create/json";
+export * from "@/request/parse-body/text";
+export * from "@/request/parse-body/json";
 
 export * from "@/response/parse-text-body";
 export * from "@/response/parse-json-body";
